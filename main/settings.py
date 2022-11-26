7380cc15c9539d1744a79f12453ccdff6204b9de
Altynai <urmatovnaa05@gmail.com>
2022-11-26 23:01:20 +0600

    add new urls and settings

--- main/settings.py
+++ main/settings.py
@@ -144,12 +144,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10000
 }
