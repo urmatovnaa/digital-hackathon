5b898f08d53a53f4345c7421853aefa54cc2767b
Aleka <mergesevaalia@gmail.com>
2022-11-26 23:14:49 +0600

    added admin.py

--- event_app/admin.py
+++ event_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from event_app.models import Category, Speaker, Event
 
-# Register your models here.
+
+@admin.register(Category)
+class ContactsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Speaker)
+class ContactsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Event)
+class ContactsAdmin(admin.ModelAdmin):
+    pass
